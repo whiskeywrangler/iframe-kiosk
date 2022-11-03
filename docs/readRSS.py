5016376bb0d2415b52a8ee945f6b788e6273a61e
whiskeywrangler <wpbachmann559@gmail.com>
2022-11-03 10:27:31 -0500

    remove added mainposterurl

--- docs/readRSS.py
+++ docs/readRSS.py
@@ -6,13 +6,14 @@ import numpy as np
 d = feedparser.parse('https://www.missingkids.org/missingkids/servlet/XmlServlet?act=rss&LanguageCountry=en_US&orgPrefix=NCMC&state=AL')
 
 links = []
-link = '/mainposter/#'
+# link = '/mainposter/#'
 
 for entry in d.entries:
     if (time.time() - time.mktime(entry.published_parsed)) >= 2592000:
         links.append(entry.link)
 
-urls = [i + link for i in links]
+# urls = [i + link for i in links]
+urls = links
 delaySeconds = np.full_like(urls, 15, dtype=int)
 delaySecJ = delaySeconds.tolist()
 
