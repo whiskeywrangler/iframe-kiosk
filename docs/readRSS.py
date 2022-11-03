79446bbfeef1e9692a76396a37faf52fb9bb71c4
whiskeywrangler <wpbachmann559@gmail.com>
2022-11-03 10:36:26 -0500

    Re-enable /mainposter addition to url

--- docs/readRSS.py
+++ docs/readRSS.py
@@ -6,14 +6,13 @@ import numpy as np
 d = feedparser.parse('https://www.missingkids.org/missingkids/servlet/XmlServlet?act=rss&LanguageCountry=en_US&orgPrefix=NCMC&state=AL')
 
 links = []
-# link = '/mainposter/#'
+link = '/mainposter'
 
 for entry in d.entries:
     if (time.time() - time.mktime(entry.published_parsed)) >= 2592000:
         links.append(entry.link)
 
-# urls = [i + link for i in links]
-urls = links
+urls = [i + link for i in links]
 delaySeconds = np.full_like(urls, 15, dtype=int)
 delaySecJ = delaySeconds.tolist()
 
