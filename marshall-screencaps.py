878edc82ba0e4fee77bf613a316afbfa0b4f2fbd
whiskeywrangler <wpbachmann559@gmail.com>
2022-11-16 16:19:52 -0600

    update marshall pdfs to png

--- marshall-screencaps.py
+++ marshall-screencaps.py
@@ -5,6 +5,7 @@ import glob
 import json
 import re
 import numpy as np
+import tempfile
 from PIL import Image
 from urllib.request import urlretrieve
 from pdf2image import convert_from_path
@@ -65,46 +66,28 @@ for f in clean_marshalls:
 for link in pdf_links:
     file_name = "/mnt/c/repos/iframe-kiosk/marshall-posters/marshall-poster-" + str(uuid.uuid4()) + ".pdf"
     urlretrieve(link, file_name)
-    """
-    browser.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/button").click()
-    time.sleep(2)
-    article = browser.find_element(By.XPATH, "/html/body/div[3]/main/div/div/div/article/div")
-    #article = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/main/div/div/div/article")))
-    file_path = "/mnt/c/repos/iframe-kiosk/marshall-posters/"
-    current_file_name = "/mnt/c/repos/iframe-kiosk/marshall-posters/marshall-poster-" + str(uuid.uuid4()) + ".png"
-    file_name = current_file_name[-56:]
-    SS.full_Screenshot(browser, file_path, file_name)
-    """
-"""    
-# Get list of current posters then crop and resize them
-current_posters = glob.glob('/mnt/c/repos/iframe-kiosk/marshall-posters/*')
-for i in current_posters:
-    left = 0
-    top = 100
-    right = 785
-    bottom = 1920
-    img = Image.open(i)
-    img.crop((left, top, right, bottom)).save(i)
-"""
+
 # Close the browser
 browser.quit()
 
 # Generate json for iframe-kiosk
 marshall_path = '/mnt/c/repos/iframe-kiosk/marshall-posters/'
 marshall_json = []
-file_list = glob.glob(marshall_path + '*')
-"""
+pdf_list = glob.glob(marshall_path + '*')
 # Convert from pdf to png
-for i in file_list:
-    image = convert_from_path(pdf_path=i, dpi=200)
-    image.save(i, 'PNG')
-"""
+for i in pdf_list:
+    with tempfile.TemporaryDirectory() as path:
+        images = convert_from_path(i, output_folder=marshall_path, fmt="png")
 
-final_list = [i.replace('/mnt/c/repos/iframe-kiosk', '.') for i in file_list]
-
+png_list = glob.glob(marshall_path + "*.png")
+final_list = [i.replace('/mnt/c/repos/iframe-kiosk', '.') for i in png_list]
+# Cleanup pdfs
+pdf_cleanup_list = glob.glob(marshall_path +'*.pdf')
+for f in pdf_cleanup_list:
+    os.remove(f)
+# append paths to json list
 for f in final_list:
-    add_pdf_vars = f + "#toolbar=0&scrollbar=0&statusbar=0&navpanes=0"
-    marshall_json.append(add_pdf_vars)
+    marshall_json.append(f)
 
 delaySeconds = np.full_like(marshall_json, 15, dtype=int)
 delaySecJ = delaySeconds.tolist()
